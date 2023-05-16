f88a72a2b44157aff5f1fcd36ebf90bff8d268a7
cobowdesiles <133759389+cobowdesiles@users.noreply.github.com>
2023-05-16 12:33:01 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,16 @@
-import Link from 'next/link';
-import { getPosts } from '../utils/mdx-utils';
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Layout, { GradientBackground } from '../components/Layout';
-import ArrowIcon from '../components/ArrowIcon';
-import { getGlobalData } from '../utils/global-data';
-import SEO from '../components/SEO';
-
-export default function Index({ posts, globalData }) {
+export default function Home() {
   return (
-    <Layout>
-      <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
-      <main className="w-full">
+    <div className="container">
+      <Head>
+        <title>💙 LETS GROW !</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
         <h1 style="text-align: left;"><strong>Faites d&eacute;coller vos projets avec Marie Ollier</strong></h1>
 <h2 style="text-align: center;"><strong>Vous cherchez un <a href="https://www.marieollier.com/graphiste-illustratrice-bordeaux/">graphiste &agrave; Bordeaux</a>&nbsp;qui puisse vous offrir des prestations de qualit&eacute; ?&nbsp;</strong></h2>
 <p><strong>Le studio de graphisme et illustration de <a title="Marie Ollier" href="https://www.marieollier.com/">Marie Ollier</a> vous propose :&nbsp;</strong></p>
@@ -30,23 +27,10 @@ export default function Index({ posts, globalData }) {
 <p style="text-align: center;"><strong>Offrez vous la cr&eacute;ativit&eacute; sans limite de Marie Ollier</strong></p>
 <p style="text-align: center;">&nbsp;</p>
 <p>&nbsp;</p>
+    
       </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
-    </Layout>
-  );
-}
 
-export function getStaticProps() {
-  const posts = getPosts();
-  const globalData = getGlobalData();
-
-  return { props: { posts, globalData } };
+      <Footer />
+    </div>
+  )
 }
